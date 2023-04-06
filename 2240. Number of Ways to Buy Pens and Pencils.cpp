df5732b4a04c0f1d2ff22303e0d65d65a55abc17
Aman Kumar Tiwari <96365707+aman086@users.noreply.github.com>
2023-04-06 20:27:26 +0530

    Update 2240. Number of Ways to Buy Pens and Pencils.cpp

--- 2240. Number of Ways to Buy Pens and Pencils.cpp
+++ 2240. Number of Ways to Buy Pens and Pencils.cpp
@@ -19,3 +19,35 @@ public:
         return finalCount;
     }
 };
+
+
+
+// this below code gives TLE
+
+
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        
+        long long finalCount = 0;
+
+        long long penPrice = 0;
+
+        while(penPrice <= total){
+
+            long long remaining = total - penPrice;
+            long long pencilPrice = 0;
+            long long pencilCount = 0;
+            
+            while(pencilPrice <= remaining){
+                pencilCount++;
+                pencilPrice += cost2;
+            }
+
+            finalCount += pencilCount;
+            penPrice += cost1;
+        }
+
+        return finalCount;
+    }
+};
